bf93283b6a2504125541dca5103e8044283bad2a
Angel Ruiz <angel.ruiz@forteinnovation.mx>
2021-01-22 17:26:24 -0600

    Initial Commit

---
+++ BraintreePayPalIntegration/BraintreePayPalIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraintreePayPalIntegrationApp.swift
+//  BraintreePayPalIntegration
+//
+//  Created by Forte Apps on 22/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BraintreePayPalIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BraintreePayPalIntegration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BraintreePayPalIntegration
+//
+//  Created by Forte Apps on 22/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
